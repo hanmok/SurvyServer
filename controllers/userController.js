ee93c717f952541e4eaadd58711d9e080e3e60c1
hanmok <dmammmm@naver.com>
2023-04-09 22:50:29 +0900

    Add .gitignore

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res, next) => { 
+	try { 
+		const [users, _] = await User.findAll();
+		res.status(200).json({count: users.length, users});
+	} catch (error) { 
+		console.log(error);
+		next(error);
+	}
+};
+
+exports.createUser = async (req, res, next) => { 
+	try { 
+		let {username, password, age, isMale} = req.body;
+		let user = new User(username, password, age, isMale);
+		user = await user.save();
+		res.status(201).json({message: "User created"});
+	} catch (error) { 
+		console.log(error);
+		next(error);
+	}
+};
+
+exports.getUserById = async (req, res, next) => { 
+	try { 
+		let userId = req.params.id;
+		let [user, _] = await User.findById(userId);
+		res.status(200).json({user: user[0]});
+	} catch (error) { 
+		console.log(error);
+		next(error);
+	}
+}
