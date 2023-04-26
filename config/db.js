599d96e13f5800af5795813faca88b28138226d8
hanmok <dmammmm@naver.com>
2023-04-27 08:44:39 +0900

    roll back, to use defined db address

--- config/db.js
+++ config/db.js
@@ -22,9 +22,9 @@ const pool = mysql.createPool({
 //     ||   'mysql://' + process.env.DB_USER+ ':' + process.env.DB_PASS 
 //         + '@' + process.env.DB_HOST+'/' + process.env.DB_NAME
 
-// const dburl = process.env.MYSQL_ADDON || 'mysql://bce8ef11b95d3a:c3fa51f1@us-cdbr-east-06.cleardb.net/heroku_3df4ab91447196b'
-const dburl = process.env.MYSQL_ADDON || 'mysql://' + process.env.DB_USER +':' +process.env.DB_PASSWORD+ '@' +process.env.DB_HOST+'/heroku_3df4ab91447196b'
-
+const dburl = process.env.MYSQL_ADDON || 'mysql://bce8ef11b95d3a:c3fa51f1@us-cdbr-east-06.cleardb.net/heroku_3df4ab91447196b'
+// const dburl = process.env.MYSQL_ADDON || 'mysql://' + process.env.DB_USER +':' +process.env.DB_PASSWORD+ '@' +process.env.DB_HOST+'/heroku_3df4ab91447196b'
+console.log(`process.env: ${process.env}`);
 console.log(`db connected to ${dburl}`);
 
 const conn = mysql.createConnection(dburl);
