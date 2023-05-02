7dca8684f42237936a66a931b7fc5b4d2b4cd2f0
hanmok <dmammmm@naver.com>
2023-05-02 23:05:54 +0900

    add reconnecting db code

--- config/db.ts
+++ config/db.ts
@@ -29,25 +29,25 @@ console.log(`try db connection to ${dburl}`);
 
 const db = mysql.createConnection(dburl);
 
-// function handleDisconnect() {
-// 	db.connect(function(err) {            
-// 	  if(err) {                            
-// 		console.log('error when connecting to db:', err);
-// 		setTimeout(handleDisconnect, 2000); 
-// 	  }                                   
-// 	});                                 
+function handleDisconnect() {
+	db.connect(function(err) {            
+	  if(err) {                            
+		console.log('error when connecting to db:', err);
+		setTimeout(handleDisconnect, 2000); 
+	  }                                   
+	});                                 
 										   
-// 	db.on('error', function(err) {
-// 	  console.log('db error', err);
-// 	  if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-// 		return handleDisconnect();                      
-// 	  } else {                                    
-// 		throw err;                              
-// 	  }
-// 	});
-//   }
+	db.on('error', function(err) {
+	  console.log('db error', err);
+	  if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+		return handleDisconnect();                      
+	  } else {                                    
+		throw err;                              
+	  }
+	});
+  }
   
-//   handleDisconnect();
+  handleDisconnect();
 
 
 module.exports = db.promise();
