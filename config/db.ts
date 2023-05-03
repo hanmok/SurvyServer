04a6704ff1a0dfd56757795294315914c09f9411
hanmok <dmammmm@naver.com>
2023-05-03 19:27:33 +0900

    Modify DB..1

--- config/db.ts
+++ config/db.ts
@@ -1,22 +1,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-	host: process.env.DB_HOST, 
-	user: process.env.DB_USER,
-	database: process.env.DB_NAME,
-	password:process.env.DB_PASSWORD
-});
-
-// let sql = "SELECT * FROM posts";
-
-// pool.execute(sql, function(err, result) { 
-// 	if (err) throw err;
-// 	result.forEach(res => { 
-// 		console.log(res.title);
-// 	})
-// 	console.log(result);
+// const pool = mysql.createPool({
+// 	host: process.env.DB_HOST, 
+// 	user: process.env.DB_USER,
+// 	database: process.env.DB_NAME,
+// 	password:process.env.DB_PASSWORD,
+// 	waitForConnections: true,
+// 	connectionLimit: 10,
+// 	queueLimit: 0
 // });
+
+const pool = mysql.createPool({
+	host: 'us-cdbr-east-06.cleardb.net', 
+	user: 'bce8ef11b95d3a',
+	database: 'heroku_3df4ab91447196b',
+	password:'c3fa51f1',
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+});
 
 // const dburl = process.env.MYSQL_ADDON 
 //     ||   'mysql://' + process.env.DB_USER+ ':' + process.env.DB_PASS 
@@ -27,31 +30,32 @@ const dburl = process.env.MYSQL_ADDON || 'mysql://bce8ef11b95d3a:c3fa51f1@us-cdb
 console.log(`process.env: ${process.env}`);
 console.log(`try db connection to ${dburl}`);
 
-const db = mysql.createConnection(dburl);
+// const db = mysql.createConnection(dburl);
 
-function handleDisconnect() {
-	db.connect(function(err) {            
-	  if(err) {                            
-		console.log('error when connecting to db:', err);
-		setTimeout(handleDisconnect, 2000); 
-	  }                                   
-	});                                 
+// function handleDisconnect() {
+// 	db.connect(function(err) {            
+// 	  if(err) {                            
+// 		console.log('error when connecting to db:', err);
+// 		setTimeout(handleDisconnect, 2000); 
+// 	  }                                   
+// 	});                                 
 										   
-	db.on('error', function(err) {
-	  console.log('db error', err);
-	  if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-		return handleDisconnect();                      
-	  } else {                                    
-		throw err;                              
-	  }
-	});
-  }
+// 	db.on('error', function(err) {
+// 	  console.log('db error', err);
+// 	  if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+// 		return handleDisconnect();                      
+// 	  } else {                                    
+// 		throw err;                              
+// 	  }
+// 	});
+//   }
   
-  handleDisconnect();
+//   handleDisconnect();
 
 
 // module.exports = db.promise();
-module.exports = db;
+// module.exports = db;
+module.exports = pool;
 // module.exports = pool.promise();
 
 // pool.execute(sql)
